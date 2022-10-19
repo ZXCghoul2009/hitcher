6d109c7391555ac2ef777012daab52be31edf9d8
ZXCghoul2009 <imfckingslave@gmail.com>
2022-10-19 08:13:42 +0300

    input with auto complete

--- src/components/pages/MainPage/MainPage.tsx
+++ src/components/pages/MainPage/MainPage.tsx
@@ -2,34 +2,80 @@ import React, {useState} from "react";
 import classes from './MainPage.module.css'
 import {Input} from "../../../UI/Input/Input";
 import {Button} from "../../../UI/Buttons/Button";
+import {Cities} from "../../../Data/Cities";
 
 
 export const MainPage: React.FC = () => {
-
-    const confirmHandler = (value: string) => {
-        console.log(formValues);
-    }
     let currentDate:any = new Date();
+
     const [formValues, setFormValue] = useState({ departure : '', arrival: '', date : currentDate, seats: '1'})
+    const [departureValue, setDepartureValue] = useState('');
+    const [arrivalValue, setArrivalValue] = useState('');
+
+
+    const filteredDepartureCities = Cities.filter(city => {
+        if (departureValue.length > 1) {
+            return city.name.toLowerCase().includes(departureValue.toLowerCase());
+        }
+    })
+
+    const filteredArrivalCities = Cities.filter(city => {
+        if (arrivalValue.length > 1) {
+            return city.name.toLowerCase().includes(arrivalValue.toLowerCase());
+        }
+    })
+
+    const cityDepartureClickHandler = (event: any) => {
+        setDepartureValue(event.target.textContent)
+
+    }
+
+    const cityArrivalClickHandler = (event: any) => {
+        setArrivalValue(event.target.textContent)
+    }
+
+
     return (
         <div className={classes.page}>
             <div className={classes.container}>
 
                     <div className={classes.input_container}>
                         <Input type="text" placeholder="Откуда"
+                               value={departureValue}
                                onChange={(event :React.ChangeEvent<HTMLInputElement>)=>{
-                                   const departure = event.target.value;
-                                   setFormValue({...formValues, departure })
+                                   setDepartureValue(event.target.value)
                                }}
                         />
+                        <ul className={classes.auto_complete}>
+                            {
+                                departureValue ? filteredDepartureCities.map((item:any) => {
+                                    return (
+                                        <li
+                                            onClick={cityDepartureClickHandler}
+                                        >{item.name} </li>
+                                )
+                            }) : null
+                            }
+                        </ul>
                     </div>
                     <div className={classes.input_container} >
                         <Input type="text" placeholder="Куда"
+                               value={arrivalValue}
                                onChange={(event :React.ChangeEvent<HTMLInputElement>)=>{
-                                   const arrival = event.target.value;
-                                   setFormValue({...formValues, arrival })
+                                   setArrivalValue(event.target.value)
                                }}
                         />
+                        <ul className={classes.auto_complete}>
+                            {
+                                arrivalValue ? filteredArrivalCities.map((item:any) => {
+                                    return (
+                                        <li
+                                            onClick={cityArrivalClickHandler}
+                                        >{item.name} </li>
+                                    )
+                                }) : null
+                            }
+                        </ul>
                     </div>
                     <div className={classes.input_container}>
                         <Input type="date"
